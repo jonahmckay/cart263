d004ce6ab45fb2236527cd9c610339897bf5bdd7
Jonah McKay <jonah@amimckay.com>
2020-02-04 21:52:41 -0500

    P1: Added projects folder and project 1 template

---
+++ projects/HappinessIsSisyphus/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/********************************************************************
+
+Happiness is Dependencies?
+Jonah McKay
+
+Project 1 for CART 263.
+Exploring the existential nature of software dependencies
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+}
