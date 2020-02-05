307fb969e605dd9d96776b6cc88b13fade86ef72
Jonah McKay <jonah@amimckay.com>
2020-02-04 22:26:04 -0500

    P1: Added random package name generators and xash logo

--- projects/HappinessIsSisyphus/js/script.js
+++ projects/HappinessIsSisyphus/js/script.js
@@ -12,7 +12,49 @@ Exploring the existential nature of software dependencies
 
 $(document).ready(setup);
 
+function generateBasePackageName()
+{
+  //Function for generating a package name that is
+  //a parody of Adobe Flash Player.
+  let companyNames = ["Clay", "Adele", "Brick", "Amoeba", "Adage", "Agape", "Addition"];
+  let conventionNames = ["Mash", "Hash", "Gash", "Bash", "Nash", "Rash"]
+  let utilityNames = ["Player"]
+
+  return generatePackageName({
+    companyNames: companyNames,
+    conventionNames: conventionNames,
+    utilityNames: utilityNames
+  })
+}
+
+function generateSubPackageName()
+{
+  //Function for generating a package name that doesn't necessarally need to be
+  //a direct parody of Adobe Flash Player.
+  let companyNames = ["Clay", "Adele", "Brick", "Amoeba", "Adage", "Agape", "Addition"];
+  let conventionNames = ["Mash", "Hash", "Gash", "Bash", "Nash", "Rash"]
+  let utilityNames = ["Player", "Sorter", "Parser", "Mediator"]
+
+  return generatePackageName({
+    companyNames: companyNames,
+    conventionNames: conventionNames,
+    utilityNames: utilityNames
+  })
+}
+
+function generatePackageName(names)
+{
+  //The names paramater ought to be an object literal that contains
+  //non-empty (//TODO: add error handler?) string arrays of companyNames,
+  //conventionNames, and utilityNames.
+
+  let companyChosen = names.companyNames[Math.floor(Math.random() * names.companyNames.length)];
+  let conventionChosen = names.conventionNames[Math.floor(Math.random() * names.conventionNames.length)];
+  let utilityChosen = names.utilityNames[Math.floor(Math.random() * names.utilityNames.length)];
+
+  return `${companyChosen} ${conventionChosen} ${utilityChosen}`;
+}
 
 function setup() {
-
+  console.log(generateBasePackageName());
 }
