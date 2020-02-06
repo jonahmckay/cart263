6350b7c539b65e23375c3e2eecbbbc143067d0b4
Jonah McKay <jonah@amimckay.com>
2020-02-06 14:42:34 -0500

    P1: added start of terms and conditions and install dialog

--- projects/HappinessIsSisyphus/js/script.js
+++ projects/HappinessIsSisyphus/js/script.js
@@ -16,6 +16,44 @@ let basePackageName;
 let basePackageDownloadStarted = false;
 let packages = [];
 
+let terms = `Let's be honest, you'll agree to whatever's written here because you need this software.
+
+The terms and conditions of using this software depend on the day of the week.
+This is legally binding, somehow. We didn't ask a lawyer,
+but we did ask one of our devs who knows a lawyer.
+
+ON MONDAYS:
+You will only use this software while your computer's CPU
+is roughly 5 degrees Celsius from overheating. Luckily our program
+is so poorly written that this should not be an issue.
+
+ON TUESDAYS:
+You will not use this software, unless you are using it.
+
+ON WEDNESDAYS:
+You must disconnect your computer from the Internet before hand.
+This isn't strictly a requirement, it's just that Wednesdays are when we push
+new updates and you've gotten too used to the way it is now to
+deal with any changes, dammit.
+
+ON THURSDAYS:
+No terms and conditions on Thursdays. Go wild! Redistribute!
+(If you do, it'll literally double our userbase)
+
+on fridays:
+typing upper case characters will result in the voiding of your usage license.
+
+ON SATURDAYS:
+Do not
+
+ON SUNDAYS:
+You may participate in our Voluntary Shady Data Collection program!
+To participate, please send files on your computer (preferably with personal
+or banking data) to our e-mail.
+
+SPECIAL!!! ON FEBRUARY 28th!!!:
+No changes but we like leap days :)`;
+
 function generateBasePackageName()
 {
   //Function for generating a package name that is
@@ -110,7 +148,7 @@ function updateDownloadProgress($packageLine)
     $packageLine.find(".installPackageButton").attr("disabled", false);
   }
 
-  $packageLine.find(".downloadProgressBar").progressbar("value", $packageLine.find(".downloadProgressBar").progressbar("value")+0.25);
+  $packageLine.find(".downloadProgressBar").progressbar("value", $packageLine.find(".downloadProgressBar").progressbar("value")+2);
   window.requestAnimationFrame(function () { updateDownloadProgress($packageLine); });
 }
 
@@ -123,9 +161,46 @@ function downloadBasePackage()
   return downloadPackage(basePackageName);
 }
 
+function installPackage()
+{
+  $(".installPackageButton").attr('disabled', true);
+  let $installDialog = $("<div id='installDialog'></div>");
+
+
+  let $installTabsContainer = $("<div id='installTabs'></div>")
+  let $installTabs = $("<ul></ul>");
+  $installTabs.append($("<li><a href='#installIntro'>Start</a></li>"));
+  $installTabs.append($("<li><a href='#installTerms'>Terms</a></li>"));
+  $installTabs.append($("<li><a href='#installFinalize'>Finish</a></li>"));
+
+  let $installIntroTab = $("<div id='installIntro'>Start</div>");
+  let $installTermsTab = $("<div id='installTerms'></br></div>");
+  let $installFinalizeTab = $("<div id='installFinalize'>Finish</div>");
+
+  $installTabsContainer.append($installTabs);
+
+  $installDialog.append($installTabsContainer);
+
+
+
+  $installTabs.append($installIntroTab);
+
+  let $termsHeader = $("<h3>Terms and Conditions</h3>");
+  let $termsText = $("<p class='termsText'></p>").css('white-space', 'pre').text(terms);
+  $installTermsTab.append($termsHeader);
+  $installTermsTab.append($termsText);
+  $installTabs.append($installTermsTab);
+  $installTabs.append($installFinalizeTab);
+
+  $('body').append($installDialog);
+  $installTabsContainer.tabs({active: 0});
+
+  $installDialog.dialog();
+
+
+}
+
 
 function setup() {
-  console.log(generateBasePackageName());
   setBasePackageName(generateBasePackageName());
-  $(".downloadProgressBar").progressbar({ value: 37 });
 }
