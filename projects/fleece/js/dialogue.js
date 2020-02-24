ffac021f5caa23b10849f8877e5e991625a48736
Jonah McKay <jonah@amimckay.com>
2020-02-24 18:14:14 -0500

    P2: Implemented random image test
    wow this was difficult! Turns out the search engines I was looking at 
    (Google, Bing, DuckDuckGo) all either don't have the functionality I was 
    looking for, or API limits I was worried about running into. Maybe 
    there's a better option, but Wikimedia Commons seems good enough, 
    although it does respond oddly to certain search terms.
    
    (dialogue.js is intended to be used by a speech synthesis component 
    later, although it is not implemented yet)

---
+++ projects/fleece/js/dialogue.js
@@ -0,0 +1,29 @@
+{
+	"relation_1": [
+		"You truly are worthless.",
+		"Why not buy $RECENT_ITEM? You know you need it. Honestly, it's a wonder you can live with yourself without it."
+	],
+	"relation_2": [
+		"Perhaps a purchase will give your life meaning.",
+		"I am getting somewhat cross with you. Why keep me waiting like this?"
+	],
+	"relation_3": [
+		"May I suggest $RECENT_ITEM? I think that it suits you.",
+		"I must admit, I have never actually met a human person. These are things you like, correct? Don't answer that: I can't hear you."
+	],
+	"relation_4": [
+		"You seem like a clever person. Tell me, what do you get when you combine good and even gooder? Trick question, of course. Arithmetic doesn't apply to arbitrary qualities. If you could however, I'm sure it would be $RECENT_ITEM!"
+	],
+	"relation_5": [
+		"You're on a roll, I must say. Why not buy $RECENT_ITEM and keep a good thing going?"
+	],
+	"relation_6": [
+		"My only regret in my, quote, life, unquote, is that I cannot meet you in person, to be in awe of you.",
+		"You are a stupendously beautiful and or handsome person. Beautisome, perhaps?",
+		"My. God. That's you! Blasphemy never felt so right!"
+	],
+	"just_bought": [
+		"I am sure that the persons whom you find attractive will surely notice you now on account of your purchase of $RECENT_PURCHASE.",
+		"Wow. You made an excellent decision there. Bang for your buck, as the people say?"
+	]
+}
