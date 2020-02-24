df13952a15d6d8665faa8e4369e16416563cb5f3
Jonah McKay <jonah@amimckay.com>
2020-02-24 18:13:01 -0500

    P1: Implemented random image test
    wow this was difficult! Turns out the search engines I was looking at (Google, Bing, DuckDuckGo) all either don't have the functionality I was looking for, or API limits I was worried about running into. Maybe there's a better option, but Wikimedia Commons seems good enough, although it does respond oddly to certain search terms.
    
    (dialogue.js is intended to be used by a speech synthesis component later, although it is not implemented yet)

--- projects/fleece/js/script.js
+++ projects/fleece/js/script.js
@@ -20,9 +20,100 @@ and everything scraped by search engines on the Internet.
 
 *********************************************************************/
 
+const IMAGES_PER_QUERY = 10;
+
 $(document).ready(setup);
 
+function getImageFromQuery(callback, query)
+{
+  //Presently uses the Wikimedia Commons image search API.
+  //Code based on https://codesnippet.io/wikipedia-api-tutorial/
+  //and wikimedia search query based on
+  //https://stackoverflow.com/a/52005541
+
+  let xhr = new XMLHttpRequest();
+
+  //Generates an image search query using a template
+  let url = `https://commons.wikimedia.org/w/api.php?action=query&generator=search&gsrnamespace=6&gsrsearch=${query}%20filetype:image&gsrlimit=${IMAGES_PER_QUERY}&prop=imageinfo&iiprop=url|mime|thumbmime&format=json&origin=*`
+
+  xhr.open('GET', url, true);
+
+  // Once request has loaded...
+  xhr.onload = function() {
+      // Parse the request into JSON
+      let data = JSON.parse(this.response);
+
+      //Choose a random image from those returned in the query.
+      let imageChosen = data.query.pages[Object.keys(data.query.pages)[Math.floor(Math.random()*Object.keys(data.query.pages).length)]].imageinfo[0].url;
+
+      //Runs the callback function provided with the file URL provided as the
+      //first argument, and the query used as the second.
+      callback(imageChosen, query);
+  }
+  // Send request to the server asynchronously
+  xhr.send();
+}
+
+function sendRandomQuery(callback)
+{
+  //get random common word TODO: make this more complicated, only a placeholder!
+  //Words lists are stored in words.js
+  let query = common.commonWords[Math.floor(Math.random()*common.commonWords.length)];
+  getImageFromQuery(callback, query)
+}
+
+function addItemtoPage(imageUrl, name)
+{
+  //Adds a shop item to the page using the imageurl given as well as the name
+  //of the item.
+  $('body').append(`<img src=${generateThumbnailLink(400, imageUrl)}></img>`);
+}
+
+function addRandomItemToPage()
+{
+  //Creates a random item, then adds it to the page.
+  sendRandomQuery(addItemtoPage);
+}
+
+function generateThumbnailLink(size, url)
+{
+  //Generates a wikimedia thumbnail link based on the URL
+  //given and the size provided.
+  //Not exactly future proof given that it relies on the way
+  //Wikimedia Commons stores its images internally, but
+  //I could find no way of doing this through their API.
+
+  //Splits the URL string into its subdomains.
+  let splitString = url.split("/");
+
+  //Search the string for the commons subdomain, and then append
+  // the /thumb domain to it.
+  for (let i = 0; i < splitString.length; i++)
+  {
+    if (splitString[i] === "commons")
+    {
+      splitString[i] =  "commons/thumb";
+    }
+  }
+
+  //Gets the filename of the image
+  let fileName = splitString[splitString.length-1];
+
+  //Generates the thumbnail filename based on the filename of the image.
+  //It is always "{thumbnailSize}px-{fileName}".
+  let thumbnailName = `${size}px-${fileName}`;
+
+  //Push the thumbnailName to the domains array, putting it at the end
+  //past the filename component.
+  splitString.push(thumbnailName);
+
+  //Reconstruct the URL.
+  let newUrl = splitString.join("/");
+
+  //Return the URL.
+  return newUrl;
+}
 
 function setup() {
-
+  addRandomItemToPage();
 }
