0d1ede6469a8629f1115cf6f1f640d988accc80a
Jonah McKay <jonah@amimckay.com>
2020-03-26 14:55:18 -0300

    A5: Added plant-symphony template

---
+++ assignments/plant-symphony/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/********************************************************************
+
+Plant Symphony (working title)
+Jonah McKay
+
+Plant Symphony is a web application that allows for participating in the
+growth of virtual plant-like forms in a 3D space, and then using the
+plant to create music.
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+}
